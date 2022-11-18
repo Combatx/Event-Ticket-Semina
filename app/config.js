c681e629a15e4c3f0ac15c3d31e8237220583e13
CombatX <ridrdo@gmail.com>
2022-11-18 19:21:18 +0700

    fix: configuration

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@ dotenv.config();
 module.exports = {
   urlDb: process.env.URL_MONGODB_DEV,
   jwtExpiration: "24h",
-  jwtSecret: "secretdanrahasisa",
+  jwtSecret: process.env.SECRET_KODE,
   gmail: process.env.EMAIL,
   password: process.env.PASSWORD,
 };
